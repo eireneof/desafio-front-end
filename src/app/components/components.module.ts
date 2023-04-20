7eeaded459988fc6db104a9c2c58cb0a72bdb533
eireneof <you@example.com>
2023-04-20 12:37:27 -0300

    feat: import HttpClientModule at components modules and add providers

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -4,6 +4,10 @@ import { HomeComponent } from './home/home.component';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MyCollectionComponent } from './my-collection/my-collection.component';
 import { SharedModule } from './shared/shared.module';
+import { GeocodingApiService } from '../services/geocoding-api/geocoding-api.service';
+import { HttpClientModule } from '@angular/common/http';
+import { DeezerApiService } from '../services/deezer-api/deezer-api.service';
+import { CurrentWeatherDataApiService } from '../services/current-weather-data-api/current-weather-data-api.service';
 
 
 
@@ -15,10 +19,16 @@ import { SharedModule } from './shared/shared.module';
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    HttpClientModule
   ],
   exports: [
     NavbarComponent
+  ],
+  providers: [
+    GeocodingApiService,
+    DeezerApiService,
+    CurrentWeatherDataApiService
   ]
 })
 export class ComponentsModule { }
